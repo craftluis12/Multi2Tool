7a88e337d86bc5a1074e36cc987c7912dadfb4cb
youngin91 <108721444+youngin91@users.noreply.github.com>
2024-04-19 00:41:15 -0500

    Update Multi2Tool.cpp v1.1
    added a system for the user to choose which OS hes running after the OS is selected the program runs code associated to the chosen OS. This is the incomplete version of the code. 
    
    to change:
    * the refresh when pressing 0 still needs updating
    
    to add:
    * automatically determine which OS is running locally 
    * Currently only have a pentesting tools; add more of those in the future
    * code cleanup figure out a way to shorten the amount of code needed for the program. Ideally there would only be one section of code instead of two
    * whenever the user chooses the wrong OS (mistype or bad input) the code will error and ask for the user to input a valid OS

--- Multi2Tool.cpp
+++ Multi2Tool.cpp
@@ -4,105 +4,214 @@
 
 
 int main() {
-	system("Color E");
-   	bool should_run = true;
-	while(should_run){
-		std::cout << R"(
-		###############################################
-		# __  __       _ _   _ ____ _____           _ #
-		#|  \/  |_   _| | |_(_)___ \_   _|__   ___ | |#
-		#| |\/| | | | | | __| | __) || |/ _ \ / _ \| |#
-		#| |  | | |_| | | |_| |/ __/ | | (_) | (_) | |#
-		#|_|  |_|\__,_|_|\__|_|_____||_|\___/ \___/|_|#
-		###############################################
-	)";
-		std::cout << "\nWelcome Hackers\n";
-	
-		std::cout << "\nChoose a tool: ";
-		//List of Tools
-		std::cout << "\n1.Privesec_List"; 
-		std::cout << "\n2.Reverse_Shells_List"; 
-		std::cout << "\n3.hash_Identifier_List"; 
-		std::cout << "\n4.Coming Soon\n"; 
-		//end of tool list
-		
-		//to choose a tool from the list using number
-		int tool = 0;
-		std::cout << "\nAnswer: ";
-		std::cin >> tool;
-		//end of choosing tool
-  		if (tool == 0) {
-  		   	system("cls");
-			continue;
+	std::string os = "";
+	std::cout << "What operating system are you using?: ";
+	std::cin >> os;
+	if (os == "windows"){
+		//windows
+		system("cls");
+		system("Color E");
+	   	bool should_run = true;
+		while(should_run){
+			std::cout << R"(
+			###############################################
+			# __  __       _ _   _ ____ _____           _ #
+			#|  \/  |_   _| | |_(_)___ \_   _|__   ___ | |#
+			#| |\/| | | | | | __| | __) || |/ _ \ / _ \| |#
+			#| |  | | |_| | | |_| |/ __/ | | (_) | (_) | |#
+			#|_|  |_|\__,_|_|\__|_|_____||_|\___/ \___/|_|#
+			###############################################
+		)";
+			std::cout << "\nWelcome Hackers\n";
+			std::cout << "\nChoose a tool: ";
+			//List of Tools
+			std::cout << "\n1.Privesec_List"; 
+			std::cout << "\n2.Reverse_Shells_List"; 
+			std::cout << "\n3.hash_Identifier_List"; 
+			std::cout << "\n4.Coming Soon\n"; 
+			//end of tool list
+			
+			//to choose a tool from the list using number
+			int tool = 0;
+			std::cout << "\nAnswer: ";
+			std::cin >> tool;
+			//end of choosing tool
+	  		if (tool == 0) {
+	  		   	system("cls");
+				continue;
+			}
+			
+			//Prive sec file dir
+			if(tool == 1){	
+				int privsec = 0;	
+				std::cout << "\nPrivesec_List\n";
+				std::cout << "1: LinPeas.sh";
+				std::cout << "\n2: LinEnum.sh";
+				std::cout << "\n3: lse.sh";
+				
+				std::cout << "\nAnswer: ";
+				std::cin >> privsec;
+				if (privsec == 0) {
+				   system("cls");
+	  		   	   continue;
+				}
+				if(privsec == 1){
+					system("curl -s -L -O https://github.com/peass-ng/PEASS-ng/releases/latest/download/linpeas.sh");
+					should_run = false;
+				};
+				if(privsec == 2){
+					system("curl -s -L -O https://raw.githubusercontent.com/rebootuser/LinEnum/c47f9b226d3ce2848629f25fe142c1b2986bc427/LinEnum.sh");
+					should_run = false;
+				};
+				if(privsec == 3){
+					system("curl -s -L -O https://raw.githubusercontent.com/diego-treitos/linux-smart-enumeration/eb80976b1d5e4325894644e264d9f91782f29f50/lse.sh");
+					should_run = false;
+				};
+			};
+			//end Prive sec
+			
+			//Reverseshells file dir
+			if(tool == 2){
+				int reverse = 0;
+				std::cout << "\nReverse_Shells_List\n";
+				std::cout << "1: PHP_Reverse_Shell";
+				
+				std::cout << "\nAnswer: ";
+				std::cin >> reverse;
+				if (reverse == 0) {	
+					system("cls");
+	  		   		continue;
+				}
+				if(reverse == 1){
+					system("curl -s -L -O https://raw.githubusercontent.com/pentestmonkey/php-reverse-shell/8aa37ebe03d896b432c4b4469028e2bed75785f1/php-reverse-shell.php");
+					should_run = false;
+				};
+			};
+			//end reverseshell
+			
+			//Hash_Identifier file dir
+			if(tool == 3){
+				int hash = 0;
+				std::cout << "\nHash_Identifier_List\n";
+				std::cout << "1: Hash_ID.py";
+			
+				std::cout << "\nAnswer: ";
+				std::cin >> hash;
+				if (hash == 0) {
+					system("cls");
+	  		   		continue;
+				}
+				if(hash == 1){
+				system("curl -s -L -O https://raw.githubusercontent.com/blackploit/hash-identifier/master/hash-id.py");
+				should_run = false;
+				};
+			};
+			//end Hash_Identifier
+			return 0;
 		}
-		
-		//Prive sec file dir
-		if(tool == 1){	
-			int privsec = 0;	
-			std::cout << "\nPrivesec_List\n";
-			std::cout << "1: LinPeas.sh";
-			std::cout << "\n2: LinEnum.sh";
-			std::cout << "\n3: lse.sh";
+	} else if(os == "linux"){
+		//linux
+		system("clear");
+		bool should_run = true;
+		while(should_run){
+			std::cout << R"(
+			###############################################
+			# __  __       _ _   _ ____ _____           _ #
+			#|  \/  |_   _| | |_(_)___ \_   _|__   ___ | |#
+			#| |\/| | | | | | __| | __) || |/ _ \ / _ \| |#
+			#| |  | | |_| | | |_| |/ __/ | | (_) | (_) | |#
+			#|_|  |_|\__,_|_|\__|_|_____||_|\___/ \___/|_|#
+			###############################################
+		)";
+			std::cout << "\nWelcome Hackers\n";
+			std::cout << "\nChoose a tool: ";
+			//List of Tools
+			std::cout << "\n1.Privesec_List"; 
+			std::cout << "\n2.Reverse_Shells_List"; 
+			std::cout << "\n3.hash_Identifier_List"; 
+			std::cout << "\n4.Coming Soon\n"; 
+			//end of tool list
 			
+			//to choose a tool from the list using number
+			int tool = 0;
 			std::cout << "\nAnswer: ";
-			std::cin >> privsec;
-			if (privsec == 0) {
-			   system("cls");
-  		   	   continue;
-			}
-			if(privsec == 1){
-				system("curl -s -L -O https://github.com/peass-ng/PEASS-ng/releases/latest/download/linpeas.sh");
-				should_run = false;
+			std::cin >> tool;
+			//end of choosing tool
+	  		if (tool == 0) {
+	  		   	system("clear");
+				continue;
 			};
-			if(privsec == 2){
-				system("curl -s -L -O https://raw.githubusercontent.com/rebootuser/LinEnum/c47f9b226d3ce2848629f25fe142c1b2986bc427/LinEnum.sh");
-				should_run = false;
-			};
-			if(privsec == 3){
-				system("curl -s -L -O https://raw.githubusercontent.com/diego-treitos/linux-smart-enumeration/eb80976b1d5e4325894644e264d9f91782f29f50/lse.sh");
-				should_run = false;
-			};
-		};
-		//end Prive sec
-		
-		//Reverseshells file dir
-		if(tool == 2){
-			int reverse = 0;
-			std::cout << "\nReverse_Shells_List\n";
-			std::cout << "1: PHP_Reverse_Shell";
 			
-			std::cout << "\nAnswer: ";
-			std::cin >> reverse;
-			if (reverse == 0) {		   
-				system("cls");
-  		   		continue;
-			}
-			if(reverse == 1){
-				system("curl -s -L -O https://raw.githubusercontent.com/pentestmonkey/php-reverse-shell/8aa37ebe03d896b432c4b4469028e2bed75785f1/php-reverse-shell.php");
+			//Prive sec file dir
+			if(tool == 1){
+				int privsec = 0;	
+				std::cout << "\nPrivesec_List\n";
+				std::cout << "1: LinPeas.sh";
+				std::cout << "\n2: LinEnum.sh";
+				std::cout << "\n3: lse.sh";
+				
+				std::cout << "\nAnswer: ";
+				std::cin >> privsec;
+				if (privsec == 0) {
+				   system("clear");
+	  		   	   continue;
+				}
+				if(privsec == 1){
+					system("curl -s -L -O https://github.com/peass-ng/PEASS-ng/releases/latest/download/linpeas.sh");
+					should_run = false;
+				};
+				if(privsec == 2){
+					system("curl -s -L -O https://raw.githubusercontent.com/rebootuser/LinEnum/c47f9b226d3ce2848629f25fe142c1b2986bc427/LinEnum.sh");
+					should_run = false;
+				};
+				if(privsec == 3){
+					system("curl -s -L -O https://raw.githubusercontent.com/diego-treitos/linux-smart-enumeration/eb80976b1d5e4325894644e264d9f91782f29f50/lse.sh");
+					should_run = false;
+				};
+			};
+			//end Prive sec
+			
+			//Reverseshells file dir
+			if(tool == 2){
+				int reverse = 0;
+				std::cout << "\nReverse_Shells_List\n";
+				std::cout << "1: PHP_Reverse_Shell";
+				
+				std::cout << "\nAnswer: ";
+				std::cin >> reverse;
+				if (reverse == 0) {	
+					system("clear");
+	  		   		continue;
+				}
+				if(reverse == 1){
+					system("curl -s -L -O https://raw.githubusercontent.com/pentestmonkey/php-reverse-shell/8aa37ebe03d896b432c4b4469028e2bed75785f1/php-reverse-shell.php");
+					should_run = false;
+				};
+			};
+			//end reverseshell
+			
+			//Hash_Identifier file dir
+			if(tool == 3){
+				int hash = 0;
+				std::cout << "\nHash_Identifier_List\n";
+				std::cout << "1: Hash_ID.py";
+			
+				std::cout << "\nAnswer: ";
+				std::cin >> hash;
+				if (hash == 0) {
+					system("clear");
+	  		   		continue;
+				}
+				if(hash == 1){
+				system("curl -s -L -O https://raw.githubusercontent.com/blackploit/hash-identifier/master/hash-id.py");
 				should_run = false;
+				};
 			};
-		};
-		//end reverseshell
-		
-		//Hash_Identifier file dir
-		if(tool == 3){
-			int hash = 0;
-			std::cout << "\nHash_Identifier_List\n";
-			std::cout << "1: Hash_ID.py";
-		
-			std::cout << "\nAnswer: ";
-			std::cin >> hash;
-			if (hash == 0) {		   
-				system("cls");
-  		   		continue;
-			}
-			if(hash == 1){
-			system("curl -s -L -O https://raw.githubusercontent.com/blackploit/hash-identifier/master/hash-id.py");
-			should_run = false;
-			};
-		};
-		//end Hash_Identifier
-		return 0;
-	}
+			//end Hash_Identifier
+			return 0;
+		}
+	} else{
+		std::cout << "Error Unknow OS System!!";
+	};	
 	return 0;
 };
