14e103e48158e270bb0779b147879aaed1e544b3
craftluis12 <88074367+craftluis12@users.noreply.github.com>
2024-05-02 03:08:50 -0400

    Update Windows_Update.cpp

--- Windows_Update.cpp
+++ Windows_Update.cpp
@@ -1,35 +1,57 @@
 #include <fstream> 
 #include <iostream> 
 #include <string> 
+#include <regex>  
+#include <chrono>
+#include <thread>
+
+void thread_sleep();
   
 int main() 
 {
 	//grabs the version file
 	system("curl -s -L -O https://raw.githubusercontent.com/craftluis12/Multi2Tool/main/version.txt");
-	
-	std::ifstream versionfile("version.txt");
-	std::string line;
     
-    if(getline(versionfile, line)){
-    	std::string version = "";
-    	std::cout << "Version Format = v1.2.2";
-    	std::cout << "\nWhat version do you have: ";
-    	std::cin >> version;
+    bool valid = true;
+    while(valid){
+    	std::ifstream versionfile("version.txt");
+		std::string line;
     	
-        if(line == version){
-        	std::cout << "version is up to date\n";
-			std::cout << "\n  Bye!!\n";
-		}else if(line != version){
-			std::cout << "Updating..";
-			system("del Multi2Tool.cpp");
-        	system("curl -s -L -O https://raw.githubusercontent.com/craftluis12/Multi2Tool/main/Multi2Tool.cpp");
-        	std::cout << "\nDone\n";
-        	std::cout << "\n  Bye!!";
-		};
-    };	  	
+    	if(getline(versionfile, line)){
+    		system("cls");
+    		std::string version = "";
+    		std::cout << "Version Format = v1.2.2";
+    		std::cout << "\nWhat version do you have: ";
+    		
+    		std::cin >> version;
+    	
+    		std::regex pattern("^v\\d+\\.\\d+\\.\\d+$");
+    	
+    		if(std::regex_match(version, pattern)){
+    			if(line == version){
+        			std::cout << "version is up to date\n";
+					std::cout << "\n  Bye!!\n";
+					versionfile.close();
+					break;
+				}else{
+					std::cout << "Updating..";
+					system("rm Multi2Tool.cpp");
+        			system("curl -s -L -O https://raw.githubusercontent.com/craftluis12/Multi2Tool/main/Multi2Tool.cpp");
+        			std::cout << "\nDone\n";
+        			std::cout << "\n  Bye!!";
+        			versionfile.close();
+        			break;
+				};
+			}else{
+				std::cout << "Invalid version format";
+				thread_sleep();
+				continue;
+			};
+    	}; 
+	};	  	
 	  	
-    versionfile.close();
     system("del version.txt");
+    //creates a bat file to delete the script and it self
     std::ofstream batchFile("bach_del.bat");
 
     if (batchFile.is_open()) {
@@ -45,4 +67,8 @@ int main()
 	system("bach_del.bat > nul 2>&1");
     	
     return 0; 
+};
+
+void thread_sleep() { //function to sleep the program
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 };
